ec930a29c099e24a74229d3f50f53ebb32296d42
kuangc2008 <kuangc1987@gmail.com>
2015-01-24 15:55:03 +0800

    创建layout package.

---
+++ src/com/qihoo/hair/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.qihoo.hair;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qihoo.haierdemo.R;
+
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+
+    }
+}
